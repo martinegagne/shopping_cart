4494b466cda90ef2675b87d87315af62ae78f326
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-23 21:54:34 -0400

    complete exercise

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,4 +1,4 @@
-require relative 'product.rb'
+require_relative 'product.rb'
 
 class ShoppingCart
 
@@ -9,21 +9,32 @@ class ShoppingCart
     @@shoppingcart << new_product
 
     return new_product
-    
   end
 
-   def remove_product(item)
-     @@shoppingcart.delete_at(item)
-   end
-
-   def self.total_cost_bf_tax
-     total = 0
-     @@shoppingcart.each do |shopping_cart|
-       total += shopping_cart.price
-     end
-     puts "Your total before taxes is #{total}"
-  end
-
-#add up total cost after taxes
-
+#    def remove_product(item)
+#      @@shoppingcart.delete_at(item)
+#    end
+#
+#    def self.total_cost_bf_tax
+#      total = 0
+#      @@shoppingcart.each do |shopping_cart|
+#        total += shopping_cart.base_price
+#      end
+#
+#      puts "Your total before taxes is #{total}."
+#   end
+#
+#   def self.total_cost_after_tax
+#     total = 0
+#     @@shopping_cart.each do |shopping_cart|
+#       total += shopping_cart.total_price_w_tax
+#     end
+#
+#     puts "Your total after taxes is #{total}."
+#   end
+#
+#   def check_cart
+#     @@shopping_cart
+#   end
+#
 end
